1e79bffc43dcc1ddd2e2ddef7aadbaaedb94e764
AdrianDonato <67679435+AdrianDonato@users.noreply.github.com>
2021-10-04 19:58:03 +0800

    added permissions for location

--- app/src/main/java/com/mobdeve/s18/donato/adrian/contacttracingbluetoothtool/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s18/donato/adrian/contacttracingbluetoothtool/MainActivity.kt
@@ -1,12 +1,117 @@
 package com.mobdeve.s18.donato.adrian.contacttracingbluetoothtool
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.mobdeve.s18.donato.adrian.contacttracingbluetoothtool.R
 
+private const val LOCATION_PERMISSION_REQUEST_CODE = 2
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var scanButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        scanButton = findViewById(R.id.scan_button)
+        scanButton.setOnClickListener{startBleScan()}
     }
+
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    override fun onResume(){
+        super.onResume()
+        if(!bluetoothAdapter.isEnabled){
+            promptBluetoothEnable()
+        }
+    }
+
+    private fun promptBluetoothEnable(){
+        if(!bluetoothAdapter.isEnabled){
+            val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            getResult.launch(enableIntent)
+        }
+    }
+
+    private val getResult = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            val value = it.data?.getStringExtra("input")
+        }
+    }
+
+    val isLocationPermissionGranted
+        get() = hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    fun Context.hasPermission(permissionType: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permissionType) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startBleScan(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !isLocationPermissionGranted) {
+            requestLocationPermission()
+        }
+        else { /* TODO: Actually perform scan */ }
+    }
+
+    private fun requestLocationPermission() {
+        if (isLocationPermissionGranted) {
+            return
+        }
+        runOnUiThread {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Location permission required")
+            builder.setMessage("Starting from Android M (6.0), the system requires apps to be granted " +
+                    "location access in order to scan for BLE devices.")
+            builder.setCancelable(false)
+            //builder.setPositiveButton("OK", DialogInterface.OnClickListener(function = ))
+            builder.setPositiveButton(android.R.string.ok) {dialog, which
+                ->                 requestPermission(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    LOCATION_PERMISSION_REQUEST_CODE
+                )
+            }
+            builder.show()
+        }
+    }
+
+    private fun Activity.requestPermission(permission: String, requestCode: Int) {
+        ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.firstOrNull() == PackageManager.PERMISSION_DENIED) {
+                    requestLocationPermission()
+                } else {
+                    startBleScan()
+                }
+            }
+        }
+    }
+
 }
