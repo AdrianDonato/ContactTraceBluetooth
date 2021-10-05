931ebc51bcc7fc373b17d4a7ea231ca9708e8691
itsmedenver <68907210+itsmedenver@users.noreply.github.com>
2021-10-05 18:47:16 +0800

    Added Scan function

---
+++ app/src/main/java/com/mobdeve/s18/donato/adrian/contacttracingbluetoothtool/Bluetooth/BLEScanner.kt
@@ -0,0 +1,4 @@
+package com.mobdeve.s18.donato.adrian.contacttracingbluetoothtool.Bluetooth
+
+class BLEScanner {
+}
--- app/src/main/java/com/mobdeve/s18/donato/adrian/contacttracingbluetoothtool/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s18/donato/adrian/contacttracingbluetoothtool/MainActivity.kt
@@ -5,6 +5,10 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
@@ -12,17 +16,25 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.ParcelUuid
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.mobdeve.s18.donato.adrian.contacttracingbluetoothtool.R
+import java.util.*
+import java.util.Arrays.toString
+import java.util.Objects.toString
+import kotlin.properties.Delegates
 
 private const val LOCATION_PERMISSION_REQUEST_CODE = 2
 
 class MainActivity : AppCompatActivity() {
     private lateinit var scanButton : Button
+    private var serviceUUID: String by Delegates.notNull()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +43,6 @@ class MainActivity : AppCompatActivity() {
         scanButton.setOnClickListener{startBleScan()}
     }
 
-    private val bluetoothAdapter: BluetoothAdapter by lazy {
-        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        bluetoothManager.adapter
-    }
 
     override fun onResume(){
         super.onResume()
@@ -64,12 +72,9 @@ class MainActivity : AppCompatActivity() {
         return ContextCompat.checkSelfPermission(this, permissionType) ==
                 PackageManager.PERMISSION_GRANTED
     }
-
-    private fun startBleScan(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !isLocationPermissionGranted) {
-            requestLocationPermission()
-        }
-        else { /* TODO: Actually perform scan */ }
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
     }
 
     private fun requestLocationPermission() {
@@ -114,4 +119,29 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /* BLE Scanner : Should be separate kt file? */
+    /* 10/5/2021 */
+    private val bleScanner by lazy {
+        bluetoothAdapter.bluetoothLeScanner
+    }
+    private val scanSettings = ScanSettings.Builder()
+            .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+            .build()
+
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            with(result.device) {
+                Log.i( "Scan Callback","Found BLE device! Name: ${name ?: "Unnamed"}, address: $address")
+            }
+        }
+    }
+
+    private fun startBleScan(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !isLocationPermissionGranted) {
+            requestLocationPermission()
+        }
+        else { /* TODO: Actually perform scan */
+            bleScanner.startScan(null, scanSettings, scanCallback)
+        }
+    }
 }
